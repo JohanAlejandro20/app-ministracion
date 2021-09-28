f089dc4bbb857537c77a54f013f3c65b0aef5fed
johan <johanneira902@gmail.com>
2021-09-27 19:34:25 -0500

    Se agrega consumo de servicio para que el administrador  consultar las preguntas que se han realizado en su conjunto

--- src/app/component/questions-admin/questions-admin.component.ts
+++ src/app/component/questions-admin/questions-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthUserService } from 'src/app/services/auth-user.service';
+import { QuestionService } from 'src/app/services/question.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -10,13 +11,90 @@ import Swal from 'sweetalert2';
 })
 export class QuestionsAdminComponent implements OnInit {
 
-  constructor(private authService: AuthUserService, private router:Router) { }
-
+  constructor(private authService: AuthUserService, private router:Router, private questionService: QuestionService) { }
+  
+  token: any;
+  id_user:any;
+  ensemble : any;
   username!: string
+  cod_conjunto: any
+  questions: any;
 
   ngOnInit(): void {
     this.username= this.authService.usuario.nombre;
+    this.token = this.authService.token
+    this.id_user = this.authService.usuario.id;
+    this.consultEnsemble();
+    // this.consultQuestionByEnsemble();
+    
   }
+
+  consultQuestionByEnsemble(){
+
+    console.log("conjunto codigo",this.cod_conjunto);
+    
+    this.questionService.getQuestionByensemble(this.token, this.cod_conjunto).subscribe((res:any)=>{
+      console.log(res);
+      if (!res.error) {
+        this.questions = res;
+        
+        if(this.questions.length>0){
+          // this.validateQuestions = false;
+        }
+
+        
+      }
+      
+
+    }, error =>{
+      if (error.status == 401) {
+        if (this.authService.isAuthenticated()) {
+          Swal.fire('Logout', `Hola ${this.authService.usuario.nombre} el token de autenticacion ah expirado`, 'info')
+          this.authService.logout();
+          this.router.navigate(['/login']);
+        }
+      }
+
+      if (error.status == 403) {
+        Swal.fire('Acceso denegado', `Hola ${this.authService.usuario.nombre} usted no tiene permisos para realizar esta operacion`, 'info')
+        this.authService.logout();
+        this.router.navigate(['/login']);
+      }
+    })
+
+  }
+
+  consultEnsemble(){
+
+
+    this.questionService.getEnsembleByUser(this.token, this.id_user).subscribe((res: any) =>{
+      console.log(res);
+      
+      if (!res.error) {
+         this.ensemble = res.nombre_conjunto;
+         this.cod_conjunto = res.cod_conjunto;
+         console.log(this.cod_conjunto);
+         this.consultQuestionByEnsemble()
+         
+        }
+      
+    },error=>{
+
+      if (error.status == 401) {
+        if (this.authService.isAuthenticated()) {
+          Swal.fire('Logout', `Hola ${this.authService.usuario.nombre} el token de autenticacion ah expirado`, 'info')
+          this.authService.logout();
+          this.router.navigate(['/login']);
+        }
+      }
+      if (error.status == 403) {
+        Swal.fire('Acceso denegado', `Hola ${this.authService.usuario.nombre} usted no tiene permisos para realizar esta operacion`, 'info')
+        this.authService.logout();
+        this.router.navigate(['/login']);
+      }
+    })
+  }
+
 
   logout(){
     this.authService.logout();
